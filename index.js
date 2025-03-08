aa2b3493741582f9296f8cef26fa54d0a82f977d
Moses Mbuki Mutitu <moses.mutitu@student.moringaschool.com>
2025-03-08 23:21:37 +0300

    assignment complete

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// code your solution here
+// 1. saturdayFun
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+  }
+  
+  // 2. mondayWork
+  const mondayWork = function (activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+  };
+  
+  // 3. wrapAdjective
+  function wrapAdjective(flair = "*") {
+    return function (adjective = "special") {
+      return `You are ${flair}${adjective}${flair}!`;
+    };
+  }
